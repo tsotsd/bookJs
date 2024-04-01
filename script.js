cf44cf2adbd80a71c240535b6258580ab3ad0ad1
tsotsd <135599352+tsotsd@users.noreply.github.com>
2024-04-01 16:07:20 +0800

    2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-let a = undefined;
-let b = 2;
-console.log(a ?? b);
+//Простые числа
+function showPrimes(n) {
+
+    for (let i = 2; i < n; i++) {
+        if (!isPrime(i)) continue;
+        console.log(i);
+    }
+}
+function isPrime(m) {
+    for (let i = 2; i < m; i++) {
+        if (m % i == 0) return false;
+    }
+    return true;
+}
+showPrimes(10)

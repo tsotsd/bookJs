731fe0dca47082f2818c37236201dfcfa2de754a
tsotsd <tsotsd8@gmail.com>
2024-06-06 17:07:16 +0800

    282

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
+let range = {
+    from: 1,
+    to: 3,
 };
 
-let count = {
-    '123': 0,
-    '132': 0,
-    '213': 0,
-    '231': 0,
-    '321': 0,
-    '312': 0,
+// 1. вызов for..of сначала вызывает эту функцию
+range[Symbol.iterator] = function() {
+//	...она возвращает	объект итератора:
+//	2.	Далее, for..of	работает только с	этим	итератором, запрашивая у него новые значения
+return {
+    current: this.from,
+    last: this.to,
+// 3. next() вызывается на каждой итерации цикла for..of
+next() {
+//4. он должен вернуть значение в виде объекта {done:.., value :...}
+    if (this.current <= this.last) {
+    return { done:	false, value: this.current++};
+} else {
+    return { done: true };
 }
-
-for (let i = 0;  i < 10; i++) {
-    let array = [1, 2, 3];
-    shuffle(array);
-    count[array.join('')]++
+}
 };
-
-for (let key in count) {
-    console.log(`${key}: ${count[key]}`)
+};
+// теперь работает!
+for (let num of range) {
+    console.log(num); // 1, затем 2, 3, 4, 5
 }
 
 
